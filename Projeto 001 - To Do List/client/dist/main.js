7561c860d008a67f6e6fe05ef14475fdd3c0a37c
ttulov3 <87874297+ttulov3@users.noreply.github.com>
2023-02-28 11:55:10 -0300

    Add files via upload

--- Projeto 001 - To Do List/client/dist/main.js
+++ Projeto 001 - To Do List/client/dist/main.js
@@ -1,1 +1,19 @@
 "use strict";
+void function main() {
+    const btAdd = document.querySelector("header .bt-add");
+    const inpAdd = document.querySelector("header .inp-add");
+    const main = document.querySelector("main");
+    const template = document.querySelector("main .line-template");
+    if (!btAdd)
+        return console.error("O elemento 'header .bt-add' não encontrado");
+    if (!inpAdd)
+        return console.error("O elemento 'header .inp-add' não encontrado");
+    if (!(inpAdd instanceof HTMLInputElement))
+        return console.error("O elemento 'header .inp-add' não encontrado");
+    btAdd.addEventListener("click", () => {
+        if (inpAdd.value.trim() == "")
+            return;
+        const line = template.content.cloneNode(true);
+        main.append(line);
+    });
+}();
